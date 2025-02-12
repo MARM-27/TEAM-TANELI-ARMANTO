96d320f1c366c13e53ba182f53bb7b9580350d47
MARM-27 <27mb.wrk.acc@gmail.com>
2025-02-12 20:45:36 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,175 +3,158 @@
 #include <windows.h>
 using namespace std;
 
-bool gameOver;
-const int width = 25,height = 25;
-int x, y, fruitX, fruitY, score;
-int tailX[100], tailY[100];
-int nTail = 2;
-enum eDirection { STOP = 0, LEFT, RIGHT, UP, DOWN};
-eDirection dir;
+class SnakeGame {
+private:
+    bool gameOver;
+    const int width = 25, height = 25;
+    int x, y, fruitX, fruitY, score;
+    int tailX[100], tailY[100];
+    int nTail = 2;
+    enum eDirection { STOP = 0, LEFT, RIGHT, UP, DOWN };
+    eDirection dir;
 
-void gotoxy(int x, int y) 
-{
-    COORD coord;
-    coord.X = x;
-    coord.Y = y;
-    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
-}
-
-void setup()
-{
-    srand(time(0));
-    gameOver = false;
-    dir = RIGHT;
-    x = width / 2;
-    y = height / 2;
-    fruitX = rand() % (width-2) + 1;
-    fruitY = rand() % (height-2) + 1;
-
-    for (int i = 0; i < nTail; i++) 
+    void gotoxy(int x, int y) 
     {
-        tailX[i] = x - (i + 1); 
-        tailY[i] = y;
+        COORD coord;
+        coord.X = x;
+        coord.Y = y;
+        SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
     }
-}
 
-void draw()
-{
-    gotoxy(0,0);
-    for(int i=0; i<height; i++)
+    void draw() 
     {
-        cout << "#";
-        for(int j=0; j<width-2; j++)
+        gotoxy(0, 0);
+        for (int i = 0; i < height; i++) 
         {
-            if(i==0 || i==height-1)
+            cout << "#";
+            for (int j = 0; j < width - 2; j++) 
             {
-                cout << "#";
-            }
-            else if(i==y && j==x) cout << "O";
-            else if(i==fruitY && j==fruitX) cout << "F";
-            else 
-            {
-                bool print = false;
-                for(int k=0; k<nTail; k++)
-                {                    
-                    if(tailX[k] == j && tailY[k] == i)
-                    {
-                        cout << "o";
-                        print = true;
-                    }
+                if (i == 0 || i == height - 1) 
+                {
+                    cout << "#";
                 }
-                if(print != true) cout << " ";
+                else if (i == y && j == x) cout << "O";
+                else if (i == fruitY && j == fruitX) cout << "F";
+                else 
+                {
+                    bool print = false;
+                    for (int k = 0; k < nTail; k++) 
+                    {
+                        if (tailX[k] == j && tailY[k] == i) 
+                        {
+                            cout << "o";
+                            print = true;
+                        }
+                    }
+                    if (!print) cout << " ";
+                }
+            }
+            cout << "#" << endl;
+        }
+        cout << "Score : " << score;
+    }
+
+    void input() 
+    {
+        if (_kbhit()) 
+        {
+            switch (_getch()) 
+            {
+                case 'w': dir = UP; break;
+                case 's': dir = DOWN; break;
+                case 'a': dir = LEFT; break;
+                case 'd': dir = RIGHT; break;
+                case 'x': gameOver = true; break;
             }
         }
-        cout << "#" << endl;
     }
-    cout << "Score : " << score;
-}
-void input()
-{
-    if(_kbhit())
+
+    void logic() 
     {
-        switch(_getch())
+        int prevX = tailX[0], prevY = tailY[0], prev2X, prev2Y;
+        tailX[0] = x;
+        tailY[0] = y;
+        for (int i = 1; i < nTail; i++) 
         {
-            case 'w':
-                dir = UP;
-                break;
-            case 's':
-                dir =DOWN;
-                break;
-            case 'a':
-                dir = LEFT;
-                break;  
-            case 'd':
-                dir = RIGHT;
-                break;  
-            case 'x':
-                gameOver = true;         
+            prev2X = tailX[i];
+            prev2Y = tailY[i];
+            tailX[i] = prevX;
+            tailY[i] = prevY;
+            prevX = prev2X;
+            prevY = prev2Y;
         }
-
+        switch (dir) 
+        {
+            case UP: y--; break;
+            case DOWN: y++; break;
+            case LEFT: x--; break;
+            case RIGHT: x++; break;
+            default: break;
+        }
+        if (x == width - 1 || x == 0 || y == height - 1 || y == 0) gameOver = true;
+        for (int i = 0; i < nTail; i++) 
+        {
+            if (tailX[i] == x && tailY[i] == y) gameOver = true;
+        }
+        if (x == fruitX && y == fruitY) 
+        {
+            score += 10;
+            fruitX = rand() % (width - 2) + 1;
+            fruitY = rand() % (height - 2) + 1;
+            nTail++;
+        }
     }
-}
-void logic()
+
+    void gameOverScreen() 
+    {
+        system("cls");
+        gotoxy(width / 4, height / 3);
+        cout << "**************************" << endl;
+        gotoxy(width / 4, (height / 3) + 1);
+        cout << "*        GAME OVER       *" << endl;
+        gotoxy(width / 4, (height / 3) + 2);
+        cout << "**************************" << endl;
+        gotoxy(width / 4, (height / 3) + 4);
+        cout << "Final Score: " << score << endl;
+        gotoxy(width / 4, (height / 3) + 6);
+        cout << "Press any key to exit...";
+        _getch();
+    }
+
+public:
+    SnakeGame() 
+    {
+        srand(time(0));
+        gameOver = false;
+        dir = RIGHT;
+        x = width / 2;
+        y = height / 2;
+        fruitX = rand() % (width - 2) + 1;
+        fruitY = rand() % (height - 2) + 1;
+        for (int i = 0; i < nTail; i++) 
+        {
+            tailX[i] = x - (i + 1);
+            tailY[i] = y;
+        }
+        score = 0;
+    }
+
+    void run() 
+    {
+        while (!gameOver) 
+        {
+            draw();
+            input();
+            logic();
+            Sleep(50);
+        }
+        gameOverScreen();
+    }
+};
+
+int main() 
 {
-    int prevX = tailX[0];
-    int prevY = tailY[0];
-    int prev2X, prev2Y;
-    tailX[0] = x;
-    tailY[0] = y;
-    for(int i=1; i<nTail; i++)
-    {
-        prev2X = tailX[i];
-        prev2Y = tailY[i];
-        tailX[i] = prevX;
-        tailY[i] = prevY;
-        prevX = prev2X;
-        prevY = prev2Y;
-    }
-    switch(dir)
-    {
-        case UP:
-            y--;
-            break;
-        case DOWN:
-            y++;
-            break;
-        case LEFT:
-            x--;
-            break;
-        case RIGHT:
-            x++;
-            break;   
-        default:
-            break;         
-    }
-
-    if(x == width-1 || x==0 || y == height-1 || y == 0) gameOver = true;
-
-    for(int i=-0; i<nTail; i++)
-    {
-        if(tailX[i] == x && tailY[i] == y)
-        gameOver = true;
-    }
-    if(x==fruitX && y==fruitY)
-    {
-        score+=10;
-        fruitX = rand() % (width-2)+1;
-        fruitY = rand() % (height-2)+1;
-        nTail++;
-    }
-}
-
-void gameOverScreen()
-{
-    gotoxy(width / 3, height / 2); 
-    cout << "=============" << endl;
-    gotoxy(width / 3, (height / 2) + 1);
-    cout << " GAME OVER " << endl;
-    gotoxy(width / 3, (height / 2) + 2);
-    cout << "=============" << endl;
-    gotoxy(width / 3, (height / 2) + 4);
-    cout << "Final Score: " << score << endl;
-    for(int i=0; i<15; i++)
-    {
-        cout << endl;
-    }
-}
-
-
-int main()
-{
-    
-    setup();
-    while(gameOver!=true)
-    {
-        draw();
-        input();
-        logic();
-        Sleep(50);
-    }
-
-    gameOverScreen();
-
+    SnakeGame game;
+    game.run();
     return 0;
 }
