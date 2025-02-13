84534d6135aaa4edc6032c19ece09aae6db130c6
MARM-27 <27mb.wrk.acc@gmail.com>
2025-02-13 13:25:08 +0530

    main.cpp

--- main.cpp
+++ main.cpp
@@ -6,10 +6,12 @@ using namespace std;
 class SnakeGame {
 private:
     bool gameOver;
-    const int width = 25, height = 25;
-    int x, y, fruitX, fruitY, score;
+    const int width = 50, height = 25; // Updated grid size
+    int x, y, score;
     int tailX[100], tailY[100];
     int nTail = 2;
+    int fruitX[4], fruitY[4]; // Store multiple food positions
+    int speed;
     enum eDirection { STOP = 0, LEFT, RIGHT, UP, DOWN };
     eDirection dir;
 
@@ -19,6 +21,15 @@ private:
         coord.X = x;
         coord.Y = y;
         SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+    }
+
+    void hideCursor()
+    {
+        HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+        CONSOLE_CURSOR_INFO cursorInfo;
+        cursorInfo.bVisible = false;
+        cursorInfo.dwSize = 1;
+        SetConsoleCursorInfo(consoleHandle, &cursorInfo);
     }
 
     void draw() 
@@ -34,7 +45,6 @@ private:
                     cout << "#";
                 }
                 else if (i == y && j == x) cout << "O";
-                else if (i == fruitY && j == fruitX) cout << "F";
                 else 
                 {
                     bool print = false;
@@ -43,6 +53,14 @@ private:
                         if (tailX[k] == j && tailY[k] == i) 
                         {
                             cout << "o";
+                            print = true;
+                        }
+                    }
+                    for (int f = 0; f < 4; f++) 
+                    {
+                        if (fruitX[f] == j && fruitY[f] == i) 
+                        {
+                            cout << "F";
                             print = true;
                         }
                     }
@@ -96,29 +114,46 @@ private:
         {
             if (tailX[i] == x && tailY[i] == y) gameOver = true;
         }
-        if (x == fruitX && y == fruitY) 
+        for (int f = 0; f < 4; f++)
         {
-            score += 10;
-            fruitX = rand() % (width - 2) + 1;
-            fruitY = rand() % (height - 2) + 1;
-            nTail++;
+            if (x == fruitX[f] && y == fruitY[f]) 
+            {
+                score += 10;
+                fruitX[f] = rand() % (width - 2) + 1;
+                fruitY[f] = rand() % (height - 2) + 1;
+                nTail++;
+            }
         }
     }
 
     void gameOverScreen() 
     {
-        system("cls");
+         system("cls");
         gotoxy(width / 4, height / 3);
-        cout << "**************************" << endl;
+        cout << "" << endl;
         gotoxy(width / 4, (height / 3) + 1);
         cout << "*        GAME OVER       *" << endl;
         gotoxy(width / 4, (height / 3) + 2);
-        cout << "**************************" << endl;
+        cout << "" << endl;
         gotoxy(width / 4, (height / 3) + 4);
         cout << "Final Score: " << score << endl;
         gotoxy(width / 4, (height / 3) + 6);
         cout << "Press any key to exit...";
         _getch();
+    }
+
+    void selectDifficulty()
+    {
+        int choice;
+        cout << "Select Difficulty Level:\n1. Easy\n2. Medium\n3. Hard\n";
+        cin >> choice;
+        switch (choice)
+        {
+            case 1: speed = 75; cout << "Level: Easy\n"; break;
+            case 2: speed = 50; cout << "Level: Medium\n"; break;
+            case 3: speed = 10; cout << "Level: Hard\n"; break;
+            default: speed = 100; cout << "Defaulting to Easy\n"; break;
+        }
     }
 
 public:
@@ -129,8 +164,13 @@ public:
         dir = RIGHT;
         x = width / 2;
         y = height / 2;
-        fruitX = rand() % (width - 2) + 1;
-        fruitY = rand() % (height - 2) + 1;
+        selectDifficulty();
+        hideCursor();
+        for (int i = 0; i < 4; i++) 
+        {
+            fruitX[i] = rand() % (width - 2) + 1;
+            fruitY[i] = rand() % (height - 2) + 1;
+        }
         for (int i = 0; i < nTail; i++) 
         {
             tailX[i] = x - (i + 1);
@@ -146,7 +186,7 @@ public:
             draw();
             input();
             logic();
-            Sleep(50);
+            Sleep(speed);
         }
         gameOverScreen();
     }
@@ -154,6 +194,7 @@ public:
 
 int main() 
 {
+    system("cls");
     SnakeGame game;
     game.run();
     return 0;
